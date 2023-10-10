bd61334a4ce72126c10cc66be1b0438a21c24b5b
Muhammad Fikrianto Aji <unicornhorse1@gmail.com>
2023-10-10 21:26:12 +0700

    Tambah fitur lihat semua data mahasiswa pada level kaprodi

---
+++ src/app/daftar-mahasiswa/page.tsx
@@ -0,0 +1,12 @@
+import Content from "@/components/daftar-mahasiswa/Content";
+import React from "react";
+
+const daftarMahasiswa = () => {
+  return (
+    <React.Fragment>
+      <Content />
+    </React.Fragment>
+  );
+};
+
+export default daftarMahasiswa;
